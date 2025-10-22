37e7acd47caaef956299f1930bb89780b3183f4f
mechard <mechard@42paris.fr>
2025-10-22 15:22:34 +0200

    Wed Oct 22 03:22:34 PM CEST 2025 - /home/mechard/Documents/git_trs update

--- git_trs.sh
+++ git_trs.sh
@@ -25,6 +25,7 @@ draw_bar() {
   i=1; while [ $i -le $filled ]; do printf "#"; i=$((i+1)); done
   i=1; while [ $i -le $empty ]; do printf "-"; i=$((i+1)); done
   printf "] %3d%%" "$p"
+  sleep 1000
 }
 
 # Clone avec barre
